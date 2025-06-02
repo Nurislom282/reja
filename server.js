4ffb1dbb76ba6a88bfc7d007dd98a3dfb732d36a
Nurislom <anurislom425@gmail.com>
2025-06-02 19:47:24 +0500

    feat: created author page

---
+++ server.js
@@ -0,0 +1,48 @@
+console.log("Web Serverni boshlash");
+
+const express = require("express");
+const app = express();
+const http = require('http');
+const fs = require('fs');
+const { render } = require("ejs");
+
+let user; 
+fs.readFile("database/user.json", "utf-8", (err, data) => {
+    if(err) {
+        console.log("ERROR:", err);
+    }else {
+        user = JSON.parse(data);
+    }
+});
+//1: Kirish code
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+//2: Session code
+//3: Views code
+app.set("views","views");
+app.set("view engine","ejs");
+
+//4: Runing code
+
+app.get('/author',(req, res) => {
+    res.render("author", { user: user });
+})
+
+
+app.post("/create-item",function(req, res){
+    console.log(req.body);
+    res.json({test: "succes"});
+});
+
+app.get('/', function(req , res){
+    res.render("harid");
+});
+
+
+const  server = http.createServer(app);
+let PORT = 3000;
+server.listen(PORT, function() {
+    console.log(`The server is runing succesfuly on port: ${PORT}`)
+})
